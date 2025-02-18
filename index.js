35673543199a1b90fce66b57f0e118978833a0db
Tim Kent <timkentdev@gmail.com>
2025-02-17 17:53:37 -0800

    fix: add new element in inventory array to fill id gap

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const inventory = [
   { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
   { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
   { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+  { id: 5, name: "carrots", price: 2.25, category: "vegetable", quantity: 94 },
   { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
   { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
   { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
